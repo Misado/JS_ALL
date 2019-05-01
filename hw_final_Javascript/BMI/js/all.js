7daf2a07c630e1e2b2bd75fd8b386c7fd290ecf4
Ado Lin <syaorando@gmail.com>
2019-05-01 19:19:41 +0800

    加上頁碼前備份

--- hw_final_Javascript/BMI/js/all.js
+++ hw_final_Javascript/BMI/js/all.js
@@ -1,77 +1,17 @@
-
-// var testData=[{
-//         bodyType: "理想",
-//         BMI: 20.90,
-//         weight: 70,
-//         height: 180,
-//         currentTime: "06-19-2017",
-//     },{
-//         bodyType: "理想",
-//         BMI: 20.90,
-//         weight: 60,
-//         height: 170,
-//         currentTime: "06-19-2017",
-//     },{
-//         bodyType: "過重",
-//         BMI: 20.90,
-//         weight: 120,
-//         height: 160,
-//         currentTime: "06-19-2017",
-//     },{
-//         bodyType: "過輕",
-//         BMI: 10.01,
-//         weight: 40,
-//         height: 170,
-//         currentTime: "05-01-2019",
-//     }
-// ]
-
-// // var testDataStr = JSON.stringify(testData);
-// // localStorage.setItem("result",testDataStr);
-
-// console.log(testData.length);
-// console.log(testData[1].bodyType);
-
-// var resultTableObj = document.querySelector(".resultTable");
-
-// // var resultTableStr ="";
-// // for ( var i=0; i<testData.length; i++){
-// //     console.log(testData[i].bodyType);
-// //     resultTableStr += "<tr>";
-// //     resultTableStr += "<td class='typeClassFine'>"+testData[i].bodyType+"</td>";
-// //     resultTableStr += "<td><span class='smallText'>BMI</span>"+testData[i].BMI+"</td>";
-// //     resultTableStr += "<td><span class='smallText'>weight</span>"+testData[i].weight+"kg</td>";
-// //     resultTableStr += "<td><span class='smallText'>height</span>"+testData[i].height+"cm</td>";
-// //     resultTableStr += "<td class='dateText'>"+testData[i].currentTime+"</td>";
-// //     resultTableStr += "</tr>";
-// //     console.log(resultTableStr);
-// // }
-
-// // resultTableObj.innerHTML = resultTableStr;
-
-// var testDataStr = localStorage.getItem("result");
-// var testDataArray = JSON.parse(testDataStr) || [];
-
-// var resultTableStr ="";
-// for ( var i=0; i<testDataArray.length; i++){
-//     console.log(testDataArray[i].bodyType);
-//     resultTableStr += "<tr>";
-//     resultTableStr += "<td class='typeClassFine'>"+testDataArray[i].bodyType+"</td>";
-//     resultTableStr += "<td><span class='smallText'>BMI</span>"+testDataArray[i].BMI+"</td>";
-//     resultTableStr += "<td><span class='smallText'>weight</span>"+testDataArray[i].weight+"kg</td>";
-//     resultTableStr += "<td><span class='smallText'>height</span>"+testDataArray[i].height+"cm</td>";
-//     resultTableStr += "<td class='dateText'>"+testDataArray[i].currentTime+"</td>";
-//     resultTableStr += "</tr>";
-//     console.log(resultTableStr);
-// }
-
-// resultTableObj.innerHTML = resultTableStr;
+/* ------ 主程式 ------ */
 
 
-// ------ 上面都是測試的code ------ //
+// 按下看結果按鈕
+var sendBtnObj = document.querySelector(".sendBtn");
+sendBtnObj.addEventListener("click",getInputData);
 
+// 顯示結果的table
 var resultTableObj = document.querySelector(".resultTable");
 resultTableObj.addEventListener("click",removeData);
+
+var dataInputHeightObj = document.getElementById("dataInputHeight");
+var dataInputWeightObj = document.getElementById("dataInputWeight");
+
 
 // 宣告BMI嚴重程度跟要套用的class
 var bodyTypeArray = [{
@@ -93,36 +33,35 @@ var bodyTypeArray = [{
     bodyType: "體重過輕",
     bodyTypeClass: "bodyTypeClassLight", 
 },{
-    bodyType: "重度過輕",
-    bodyTypeClass: "bodyTypeClassHeavy", 
+    bodyType: "中度過輕",
+    bodyTypeClass: "bodyTypeClassMedium", 
 },{
     bodyType: "嚴重過輕",
     bodyTypeClass: "bodyTypeClassSerious", 
 }];
 
+// 一開始要先顯示資料
 showData();
 
-var dataInputHeightObj = document.getElementById("dataInputHeight");
-var dataInputWeightObj = document.getElementById("dataInputWeight");
-
-var sendBtnObj = document.querySelector(".sendBtn");
-
-sendBtnObj.addEventListener("click",getInputData);
 
 
+/* ------ 函式宣告 ------ */
+
+
+// 輸入值按下按鈕後要做的事
 function getInputData(){
-    
+    // 取得現在時間
     var today = new Date();
     var todayMonth = today.getMonth()+1;
-    todayMonth = JSON.stringify(todayMonth);
+    todayMonth = JSON.stringify(todayMonth); //要轉字串才能知道長度
     var todayDay = today.getDate();
-    todayDay = JSON.stringify(todayDay);
-    console.log("todayMonth.length: "+todayMonth.length);
+    todayDay = JSON.stringify(todayDay); //要轉字串才能知道長度
+    // 日期如果只有1碼前面補0
     if ( todayMonth.length == 1 ){ todayMonth = "0"+todayMonth; }
     if ( todayDay.length == 1 ){ todayDay = "0"+todayDay; }
     var currentDateTime =(todayMonth)+"-"+todayDay+"-"+today.getFullYear();
 
-
+    // 如果沒輸入值就跳警告視窗，後面不做
     if ( dataInputHeightObj.value == "" ){
         alert("請輸入身高！");
         return;
@@ -131,18 +70,13 @@ function getInputData(){
         alert("請輸入體重！");
         return;
     }
-
-    console.log(dataInputHeightObj.value);
     
     var dataInputHeight = parseInt(dataInputHeightObj.value);
     var dataInputWeight = parseInt(dataInputWeightObj.value);
 
-    console.log(dataInputHeight);
-    console.log(dataInputWeight);
-
     var BMICalculateResult = dataInputWeight / (dataInputHeight/100 * dataInputHeight/100);
     BMICalculateResult = BMICalculateResult.toFixed(2); //取到小數點第2位
-    console.log("BMIValue: "+BMICalculateResult);
+
     var bodyType = "";
     if ( BMICalculateResult >= 40 ){ bodyType = "嚴重肥胖"; } 
     else if ( BMICalculateResult < 40 && BMICalculateResult >= 35 ){ bodyType="重度肥胖"; }
@@ -150,24 +84,21 @@ function getInputData(){
     else if ( BMICalculateResult < 30 && BMICalculateResult >= 25 ){ bodyType="輕度肥胖"; }
     else if ( BMICalculateResult < 25 && BMICalculateResult >= 18.5 ){ bodyType="體重正常"; }
     else if ( BMICalculateResult < 18.5 && BMICalculateResult >= 16 ){ bodyType="體重過輕"; }
-    else if ( BMICalculateResult < 16 && BMICalculateResult >= 15 ){ bodyType="重度過輕"; }
+    else if ( BMICalculateResult < 16 && BMICalculateResult >= 15 ){ bodyType="中度過輕"; }
     else { bodyType="嚴重過輕"; }
 
+    //把取到的資料傳給增加資料的函數
     addInputData(dataInputHeight,dataInputWeight,BMICalculateResult,bodyType,currentDateTime);
 
+    // 清空輸入資料
     dataInputHeightObj.value = "";
     dataInputWeightObj.value = ""
 }
 
+// 增加輸入的資料
 function addInputData(heightValue,weightValue,BMIValue,bodyTypeValue,currentDateTime){
-    console.log(heightValue+" "+weightValue+" "+BMIValue+" "+bodyTypeValue+" "+currentDateTime);
-
     var localStorageDataStr = localStorage.getItem("result");
     var localStorageDataArray = JSON.parse(localStorageDataStr) || [];
-
-    // for ( var i=0; i<localStorageDataArray.length; i++){
-    //     console.log(localStorageDataArray[i].bodyType);
-    // }
 
     localStorageDataArray.push({
         bodyType: bodyTypeValue,
@@ -177,24 +108,24 @@ function addInputData(heightValue,weightValue,BMIValue,bodyTypeValue,currentDate
         currentTime: currentDateTime,
     });
     
-
     localStorageDataStr = JSON.stringify(localStorageDataArray);
     localStorage.setItem("result",localStorageDataStr);
 
     showData();
 }
 
+// 顯示資料
 function showData(){
-    
     var localStorageDataStr = localStorage.getItem("result");
     var localStorageDataArray = JSON.parse(localStorageDataStr);
 
-    if ( localStorageDataArray == null ){return;}
+    if ( localStorageDataArray == null ){return;} //取到的值為null就跳出不做
 
     var resultTableStr = "" ;
     var bodyTypeClass = "" ;
+
+    // 比對嚴重程度找到要套的class
     for ( var i=0; i<localStorageDataArray.length; i++){
-        console.log(localStorageDataArray[i].bodyType);
         for ( var j=0; j<bodyTypeArray.length;j++){
             if ( localStorageDataArray[i].bodyType == bodyTypeArray[j].bodyType ){
                 bodyTypeClass = bodyTypeArray[j].bodyTypeClass;
@@ -208,25 +139,17 @@ function showData(){
         resultTableStr += "<td class='dateText'>"+localStorageDataArray[i].currentTime+"</td>";
         resultTableStr += "<td><input type='button' class='pure-button pure-button-primary pure-button-customized' value='刪除' data-num='"+i+"'></td>";
         resultTableStr += "</tr>";
-        console.log(resultTableStr);
     }
-
     resultTableObj.innerHTML = resultTableStr;
 }
 
-
-
+// 移除資料
 function removeData(event){
-    
-    if ( event.target.nodeName != "INPUT" ){return;}
-    console.log(event.target.dataset.num);
+    if ( event.target.nodeName != "INPUT" ){return;} //點到的不是INPUT就不做
 
     var removeDataNum = event.target.dataset.num;
-
     var localStorageDataStr = localStorage.getItem("result");
     var localStorageDataArray = JSON.parse(localStorageDataStr);
-
-    
 
     localStorageDataArray.splice(removeDataNum,1);
     localStorageDataStr = JSON.stringify(localStorageDataArray);
