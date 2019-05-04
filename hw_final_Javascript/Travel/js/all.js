2489c86de136ffaf5d070b4a1a63bd889973a2bd
Ado Lin <syaorando@gmail.com>
2019-05-04 11:51:19 +0800

    加上頁碼

--- hw_final_Javascript/Travel/js/all.js
+++ hw_final_Javascript/Travel/js/all.js
@@ -4,10 +4,19 @@ console.log(kaoTravelSite[0].Name);
 
 console.log(kaoTravelSite.length);
 
+var districtRecords = findDistrict(kaoTravelSite);
+
 var siteBlockObj = document.querySelector(".siteBlock");
 // console.log(siteBlockObj.innerHTML);
 var distrctTitleObj = document.getElementById("distrctTitle");
-var districtMenuOptionObj = document.querySelector(".districtMenuOption");
+
+//每頁要顯示的筆數
+var recordnumPerPage = 5;
+var currentPageNum = 1; //現在所在頁數
+var pageMenuObj = document.querySelector(".pageMenu");
+pageMenuObj.addEventListener("click",showDataPage);
+// showTotalPageMenu();
+
 
 var districtSelected = "三民區"; //先給它一個預設值顯示結果
 showDistrictSite();
@@ -38,7 +47,7 @@ function findDistrict(inputArray){
     return objAll;
 }
 
-var districtRecords = findDistrict(kaoTravelSite);
+
 
 console.log(districtRecords[0].District,districtRecords[0].Count);
 
@@ -96,43 +105,81 @@ function changeDistrictSite(event){
         return;
     }
     districtSelected = event.target.value;
+    currentPageNum = 1; //換選項後頁碼初始值一定要先設為1
     showDistrictSite();
 }
 
 function showDistrictSite(){
+
+    var totalResultNum;
+    for ( var i=0; i<districtRecords.length; i++){
+        if ( districtSelected == districtRecords[i].District ){
+            totalResultNum = districtRecords[i].Count;
+            console.log("districtSelected: "+districtSelected,"totalResultNum: "+totalResultNum);
+            break;
+        }
+    }
+
+
+
+    var recordCount = 1;
+    var fromResultNum = (currentPageNum-1)*recordnumPerPage+1; //該頁的起始點
+    var toResultNum = currentPageNum*recordnumPerPage; //該頁的結束點
+    if ( toResultNum >= totalResultNum){toResultNum = totalResultNum};
+    
+    
+
+    console.log("recordCount: "+recordCount,"fromResultNum: "+fromResultNum,"toResultNum: ",toResultNum);
+
     // console.log(event.target.value);
     console.log("districtSelected: "+districtSelected);
     distrctTitleObj.textContent = districtSelected;
     var siteBlockObjStr = "";
     for ( var i=0; i<kaoTravelSite.length; i++){
+        // console.log(i);
+        // console.log("recordCount: "+recordCount,"fromResultNum: "+fromResultNum,"toResultNum: ",toResultNum);
+        
+        
+        //     console.log("kaoTravelSite[i].Zone: "+kaoTravelSite[i].Zone);
+        //     console.log("districtSelected: "+districtSelected);
+            if ( kaoTravelSite[i].Zone == districtSelected ){
+                console.log("recordCount: "+recordCount);
+                // recordCount如果大於等於起始點 且 小於等於結束點才真的產生html
+                if ( recordCount >= fromResultNum && recordCount <= toResultNum ){
+                console.log(kaoTravelSite[i].Name);
+                console.log(kaoTravelSite[i].Picture1);
 
-        if ( kaoTravelSite[i].Zone == districtSelected ){
-            console.log(kaoTravelSite[i].Name);
-            console.log(kaoTravelSite[i].Picture1);
-
-            //圖片網址這部份設定要用跳脫字元
-            var sitePicUrl = "style='background-image:url(&quot;"+kaoTravelSite[i].Picture1+"&quot;)'>";
-            console.log("sitePicUrl: "+sitePicUrl);
-            
-            siteBlockObjStr += "<li>";
-            siteBlockObjStr += "<div class='sitePic' "+sitePicUrl+"<div class='sitePicMask'><h3>"+kaoTravelSite[i].Name+"</h3><p>"+kaoTravelSite[i].Zone+"</p></div></div>";
-            siteBlockObjStr += "<table class='siteInfo'>";
-            siteBlockObjStr += "<tr><td><img src='img/icons_clock.png'></td><td><span>"+kaoTravelSite[i].Opentime+"</span></td></tr>";
-            siteBlockObjStr += "<tr><td><img src='img/icons_pin.png'></td><td><span>"+kaoTravelSite[i].Add+"</span></td></tr>";
-            siteBlockObjStr += "<tr><td><img src='img/icons_phone.png'></td><td><span>"+kaoTravelSite[i].Tel+"</span></td></tr>";
-            siteBlockObjStr += "</table>";
-            siteBlockObjStr += "<div class='tagInfo'><img src='img/icons_tag.png'><span>"+kaoTravelSite[i].Ticketinfo+"</span></div>";
-            siteBlockObjStr += "</li>";
-        }
+                //圖片網址這部份設定要用跳脫字元
+                var sitePicUrl = "style='background-image:url(&quot;"+kaoTravelSite[i].Picture1+"&quot;)'>";
+                console.log("sitePicUrl: "+sitePicUrl);
+                
+                siteBlockObjStr += "<li>";
+                siteBlockObjStr += "<div class='sitePic' "+sitePicUrl+"<div class='sitePicMask'><h3>"+kaoTravelSite[i].Name+"</h3><p>"+kaoTravelSite[i].Zone+"</p></div></div>";
+                siteBlockObjStr += "<table class='siteInfo'>";
+                siteBlockObjStr += "<tr><td><img src='img/icons_clock.png'></td><td><span>"+kaoTravelSite[i].Opentime+"</span></td></tr>";
+                siteBlockObjStr += "<tr><td><img src='img/icons_pin.png'></td><td><span>"+kaoTravelSite[i].Add+"</span></td></tr>";
+                siteBlockObjStr += "<tr><td><img src='img/icons_phone.png'></td><td><span>"+kaoTravelSite[i].Tel+"</span></td></tr>";
+                siteBlockObjStr += "</table>";
+                siteBlockObjStr += "<div class='tagInfo'><img src='img/icons_tag.png'><span>"+kaoTravelSite[i].Ticketinfo+"</span></div>";
+                siteBlockObjStr += "</li>";
+                
+                } 
+                recordCount += 1; //只要有符合區域 recordCount+1
+                // console.log("recordCount: "+recordCount);
+            }
+        
+        // console.log("siteBlockObjStr: "+siteBlockObjStr);
+        
         
     }
     siteBlockObj.innerHTML = siteBlockObjStr;
+    showTotalPageMenu();
 }
 
 console.log("----- before -----");
 console.log(districtRecords);
 // 要做熱門TOP5，所以把陣列照數量順序排序
-districtRecords = districtRecords.sort(function (a, b) {
+districtRecordsSort = districtRecords.sort(function (a, b) {
     return a.Count < b.Count ? 1 : -1;
    });
 console.log("----- after -----");
@@ -140,7 +187,8 @@ console.log(districtRecords);
 
 
 //動態產生熱門行政區
-var districtList = document.getElementById("districtOption");
+var districtMenuOptionObj = document.querySelector(".districtMenuOption");
+districtMenuOptionObj.addEventListener("click",showHotDistrictSite);
 
 for ( var i=0; i<5; i++){
     var districtNode = document.createElement("li");
@@ -150,4 +198,106 @@ for ( var i=0; i<5; i++){
     districtNodeLink.textContent = districtRecords[i].District;
     districtNode.appendChild(districtNodeLink);
     districtMenuOptionObj.appendChild(districtNode);
+}
+
+function showHotDistrictSite(event){
+    event.preventDefault();
+    console.log(event.target.nodeName);
+    if ( event.target.nodeName != "A" ){return;}
+    console.log(event.target.textContent);
+    districtSelected = event.target.textContent;
+    currentPageNum = 1; //換選項後頁碼初始值一定要先設為1
+    showDistrictSite();
+}
+
+
+
+// 動態產生頁碼
+function showTotalPageMenu(){
+    // var localStorageDataStr = localStorage.getItem("result");
+    // var localStorageDataArray = JSON.parse(localStorageDataStr);
+    var totalResultNum = 1;
+    for ( var i=0; i<districtRecords.length; i++){
+        if ( districtSelected == districtRecords[i].District ){
+            totalResultNum = districtRecords[i].Count;
+            break;
+        }
+    }
+    // var totalResultNum = localStorageDataArray.length;
+    var totalPageNum = parseInt(totalResultNum/recordnumPerPage);
+    
+    if ( totalResultNum%recordnumPerPage > 0 ){
+        totalPageNum = totalPageNum+1;
+    }
+    
+    var pageMenuStr = "";
+
+    // 如果是第1頁，往上頁就不要有連結
+    if ( currentPageNum == 1 ){
+        pageMenuStr += "<li><a data-page='prev'>⟸</a></li>";
+    } else {
+        pageMenuStr += "<li><a href='#' class='pageMenuNum' data-page='prev'>⟸</a></li>";
+    }
+
+    for ( var i=1; i<=totalPageNum; i++){
+        // 如果 i 跟現在頁面一樣，就不要有連結，且套用樣式讓使用者知道目前在那一頁
+        if ( i == currentPageNum ){
+            pageMenuStr += "<li><a class='showCurrentPage' data-page='"+i+"'>"+i+"</a></li>";
+        } else {
+            pageMenuStr += "<li><a href='#' class='pageMenuNum' data-page='"+i+"'>"+i+"</a></li>";
+        }
+    }
+
+     // 如果是最後1頁，往下頁就不要有連結
+    if ( currentPageNum == totalPageNum ){
+        pageMenuStr += "<li><a data-page='next' >⟹</a></li>";
+    } else {
+        pageMenuStr += "<li><a href='#' class='pageMenuNum' data-page='next' >⟹</a></li>";
+    }
+    pageMenuObj.innerHTML = pageMenuStr;
+}
+
+
+
+//  點頁碼後的動作，並把現在頁碼設定成點到的頁碼值
+function showDataPage(event){
+    event.preventDefault();
+    if ( event.target.nodeName != "A" ){return;}
+
+    //如果點到的頁碼是現在的頁碼，後面就不做
+    if ( event.target.dataset.page == currentPageNum ){return;}
+
+    var totalResultNum = 1;
+    for ( var i=0; i<districtRecords.length; i++){
+        if ( districtSelected == districtRecords[i].District ){
+            totalResultNum = districtRecords[i].Count;
+            break;
+        }
+    }
+    var totalPageNum = parseInt(totalResultNum/recordnumPerPage);
+    console.log("未+1前的totalPageNum: "+totalPageNum);
+      
+    // 除於recordnumPerPage如果有餘數表示會跑到下一頁
+    if ( totalResultNum%recordnumPerPage > 0 ){
+        totalPageNum = totalPageNum+1;
+    }
+
+    // 點第1頁點往上就不處理，點最後1頁點往下就不處理
+    if ( currentPageNum == 1 && event.target.dataset.page == "prev" ){return;}
+    if ( currentPageNum == totalPageNum && event.target.dataset.page == "next" ){return;
+    }
+    
+    // 點往上一頁就把頁碼-1 下一頁頁碼+1 都不是就設為點到的頁數
+    if ( event.target.dataset.page == "prev"){
+        currentPageNum = currentPageNum - 1;
+    } else if ( event.target.dataset.page == "next"){
+        currentPageNum = currentPageNum + 1;
+    } else {
+        currentPageNum = parseInt(event.target.dataset.page);
+    }
+
+    console.log("totalPageNum: "+totalPageNum);
+    console.log("currentPageNum: "+currentPageNum);
+
+    showDistrictSite();
 }
