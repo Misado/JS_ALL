9af64c234d82eafa3c106f7b2536e6b396c7305e
Ado Lin <syaorando@gmail.com>
2019-07-22 09:55:15 +0800

    改成簡單的25分倒數

--- F2E/week1_改成簡單的25分倒數/js/all.js
+++ F2E/week1_改成簡單的25分倒數/js/all.js
@@ -31,6 +31,13 @@
 
 /* ------ 我是範例分隔線 END ------ */
 
+// let today=new Date();
+// let currentDateTime =today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+// console.log("currentDateTime: "+currentDateTime);
+// let startDate = new Date(currentDateTime);
+// let endDate = new Date('2019/7/22 11:59:59');
+// let spantime = (endDate - startDate)/1000;
+
 let startDate = new Date('2019/7/22 00:00:00');
 let endDate = new Date('2019/7/22 00:25:00');
 let spantime = (endDate - startDate)/1000;
@@ -38,7 +45,10 @@ let spantime = (endDate - startDate)/1000;
 
   $(document).ready(function () {
     $(".playBtn").click(function(event) {
+		/* Act on the event */
         console.log("有點到");
+        // sendBtnObj.style.display = "block";
+        // resultBtnObj.style.display = "none";
         $(".playBtn").hide();
         $(".pauseBtn").show();
         countDownPlay();
@@ -46,6 +56,7 @@ let spantime = (endDate - startDate)/1000;
     });
 
     $(".pauseBtn").click(function(event) {
+		/* Act on the event */
         console.log("點下暫停!!!!");
         // $(".playBtn").show();
         // $(".pauseBtn").hide();
@@ -56,13 +67,17 @@ let spantime = (endDate - startDate)/1000;
     function countDownPlay(){
         $(this).everyTime('1s', 'tomatoClock',function(i) {
             spantime --;
+        //    let d = Math.floor(spantime / (24 * 3600));
+        //    let h = Math.floor((spantime % (24 * 3600)) / 3600);
             let m = Math.floor((spantime % 3600) / (60));
             let s = Math.floor(spantime % 60);
     
         if (spantime > 0) {
+            // $('#hour').text(h + (d*24));
             $('#min').text(m);
             $('#sec').text(s);
         } else { // 避免倒數變成負的
+            // $('#hour').text(0);
             $('#min').text(0);
             $('#sec').text(0);
         }
