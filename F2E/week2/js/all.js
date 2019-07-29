1de17eeaa1785fbc771ec4e1f66fdf977a2aea25
Ado Lin <syaorando@gmail.com>
2019-07-29 10:30:52 +0800

    晴天娃娃會隨著滑鼠游標滑不同邊而轉不同邊

--- F2E/week2/js/all.js
+++ F2E/week2/js/all.js
@@ -1,1 +1,11 @@
-console.log("week2");
+$(document).ready(function() {
+
+    $(".frame.left").hover(function() {
+        $(".shinydollPic").removeClass("right");
+    });
+
+    $(".frame.right").hover(function() {
+        $(".shinydollPic").addClass("right");
+    });
+
+});
